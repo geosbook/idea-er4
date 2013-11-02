94a61add9f6a07cf4e6e54e94ce29033101605e2
antonio <geosbook@gmail.com>
2013-11-02 21:07:44 +0400

    finish user edit, update, index, destroy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,17 @@
 class UsersController < ApplicationController
+
+  before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
+
   def new
     @user = User.new
   end
   
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
   def show
     @user = User.find(params[:id])
   end
@@ -10,6 +19,7 @@ class UsersController < ApplicationController
   def create
     @user =  User.new(user_params)
     if @user.save
+      sign_in @user
       flash[:success] = "Welcome to IDEA=ER4"
       redirect_to @user
     else
@@ -17,11 +27,47 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      #Handle a successful update
+      flash[:success] = "Profile updated!"
+      redirect_to @user
+    else
+      render 'edit'
+    end 
+    
+  end
   
+  def destroy
+    User.find(params[:id].destroy)
+    flash[:success] = "User deleted."
+    redirect_to users_url
+  end
+ 
   private
   
    def user_params
      params.require(:user).permit(:name, :email, :password, 
                                       :password_confirmation)
+   end
+
+   def signed_in_user
+     unless signed_in?
+       store_location
+       redirect_to signin_url, notice: "Please sign in."
+     end
+   end
+   
+   def correct_user
+     @user = User.find(params[:id])
+     redirect_to(root_url)unless current_user?(@user)
+   end
+   
+   def admin_user
+     redirect_to(root_url) unless current_user.admin?
    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 IdeaEr4::Application.routes.draw do
 
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   root 'static_pages#home'
   match "/register", to: 'users#new', via: 'get'
+  match "/signin", to: 'sessions#new', via: 'get'
+  match "/signout", to: 'sessions#destroy', via: 'delete'
   match "/help", to: 'static_pages#help', via: 'get'
   match "/about", to: 'static_pages#about', via: 'get'
   match "/contact", to: 'static_pages#contact', via: 'get'
